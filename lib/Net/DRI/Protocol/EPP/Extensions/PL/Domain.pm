290280b43e9f673e0593bcb950b551fb1b9115e6
Michael Holloway <michael@thedarkwinter.com>
2014-05-19 04:02:33 +0200

    nask-poll: clean up pl domain_create

--- lib/Net/DRI/Protocol/EPP/Extensions/PL/Domain.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/PL/Domain.pm
@@ -98,47 +98,12 @@ sub create
  my ($epp,$domain,$rd)=@_;
  my $mes=$epp->message();
 
- my @d=Net::DRI::Protocol::EPP::Util::domain_build_command($mes,'create',$domain);
- my $def = $epp->default_parameters();
-
- if ($def && (ref($def) eq 'HASH') && exists($def->{domain_create}) && (ref($def->{domain_create}) eq 'HASH'))
- {
-  $rd={} unless ($rd && (ref($rd) eq 'HASH') && keys(%$rd));
-  while(my ($k,$v)=each(%{$def->{domain_create}}))
-  {
-   next if exists($rd->{$k});
-   $rd->{$k}=$v
-  }
- }
-
- ## Period, OPTIONAL
- push @d,Net::DRI::Protocol::EPP::Util::build_period($rd->{duration}) if Net::DRI::Util::has_duration($rd);
-
- ## Nameservers, OPTIONAL
- push @d,build_ns($epp,$rd->{ns},$domain) if Net::DRI::Util::has_ns($rd);
-
- ## Contacts, all OPTIONAL
- if (Net::DRI::Util::has_contact($rd))
- {
-  my $cs=$rd->{contact};
-  my @o=$cs->get('registrant');
-  push @d,['domain:registrant',$o[0]->srid()] if (@o);
-  push @d,Net::DRI::Protocol::EPP::Util::build_core_contacts($epp,$cs);
- }
-
- ## AuthInfo
- Net::DRI::Exception::usererr_insufficient_parameters("authInfo is mandatory") unless (Net::DRI::Util::has_auth($rd));
- push @d,Net::DRI::Protocol::EPP::Util::domain_build_authinfo($epp,$rd->{auth});
- $mes->command_body(\@d);
-
  return unless exists($rd->{reason}) || exists($rd->{book});
-
- my $eid=build_command_extension($mes,$epp,'extdom:create');
-
  my @e;
  push @e,['extdom:reason',$rd->{reason}] if (exists($rd->{reason}) && $rd->{reason});
  push @e,['extdom:book']                 if (exists($rd->{book}) && $rd->{book});
 
+ my $eid=build_command_extension($mes,$epp,'extdom:create');
  $mes->command_extension($eid,\@e);
  return;
 }
