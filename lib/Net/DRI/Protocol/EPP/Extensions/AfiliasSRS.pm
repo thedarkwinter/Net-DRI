f183d49bc7bc1c56ac359f75299c115d36059e15
Michael Holloway <michael@thedarkwinter.com>
2015-04-15 12:01:33 +0200

    draft-brown-epp-fees-03 (fee-0.7)

--- lib/Net/DRI/Protocol/EPP/Extensions/AfiliasSRS.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/AfiliasSRS.pm
@@ -33,7 +33,13 @@ sub setup
  return;
 }
 
-sub default_extensions { return qw/GracePeriod SecDNS LaunchPhase Afilias::IPR Afilias::IDNLanguage Afilias::Message Afilias::Registrar Afilias::Price Afilias::JSONMessage/; }
+sub default_extensions { 
+ my ($self,$pp) = @_;
+ $self->{brown_fee_version} = $pp->{brown_fee_version} if exists $pp->{brown_fee_version};
+ my @extensions = qw/GracePeriod SecDNS LaunchPhase Afilias::IPR Afilias::IDNLanguage Afilias::Message Afilias::Registrar Afilias::JSONMessage/;
+ push @extensions, (defined $self->{brown_fee_version}) ? 'CentralNic::Fee' : 'Afilias::Price';
+ return @extensions;
+}
 
 ####################################################################################################
 
