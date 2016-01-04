87d456c50cb36504a778270123a587122a662c03
Paulo Jorge <paullojorgge@gmail.com>
2016-01-04 15:41:32 +0000

    add comma

--- lib/Net/DRI/DRD/NGTLD.pm
+++ lib/Net/DRI/DRD/NGTLD.pm
@@ -637,7 +637,7 @@ Contended TLD's not included
  return {
      bep_type => 2,
      tlds => ['xn--45q11c','xn--3bst00m','xn--ses554g','xn--efvy88h','xn--czr694b','xn--czru2d','xn--6qq986b3xl','xn--30rr7y','xn--imr513n','xn--otu796d','xn--9et52u','xn--hxt814e','wang','top'],
-     # tlds => ['open']; # use this if using their OT&E. We can only test with this TLD!
+     # tlds => ['open'], # use this if using their OT&E. We can only test with this TLD!
      transport_protocol_default => ['Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::NEWGTLD',{}],
      whois_server => (defined $tld && $tld =~ m/\w+/ ? 'whois.nic.' . $tld : undef),
    } if $bep eq 'knet';
