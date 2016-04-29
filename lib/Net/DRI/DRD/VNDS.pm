f1123b3ffc78cdfeeea3841ae273fb56550cab63
Paulo Jorge <paullojorgge@gmail.com>
2016-04-29 09:33:34 +0200

    verisign added .insurance

--- lib/Net/DRI/DRD/VNDS.pm
+++ lib/Net/DRI/DRD/VNDS.pm
@@ -73,7 +73,7 @@ sub new
 
 sub periods       { return map { DateTime::Duration->new(years => $_) } (1..10); }
 sub name          { return 'VNDS'; }
-sub tlds          { return qw/com net cc tv bz jobs xn--pssy2u xn--c1yn36f xn--11b4c3d xn--t60b56a xn--c2br7g xn--42c2d9a xn--j1aef xn--3pxu8k xn--hdb9cza1b xn--mk1bu44c xn--fhbei xn--tckwe azure bank bing broker career cfd crs forex hotmail java maif markets microsoft ooo oracle pictet realtor sca shell sky spreadbetting trading xbox windows/; } ## If this changes, VeriSign/NameStore will need to be updated also
+sub tlds          { return qw/com net cc tv bz jobs xn--pssy2u xn--c1yn36f xn--11b4c3d xn--t60b56a xn--c2br7g xn--42c2d9a xn--j1aef xn--3pxu8k xn--hdb9cza1b xn--mk1bu44c xn--fhbei xn--tckwe azure bank bing broker career cfd crs forex hotmail insurance java maif markets microsoft ooo oracle pictet realtor sca shell sky spreadbetting trading xbox windows/; } ## If this changes, VeriSign/NameStore will need to be updated also
 sub object_types  { return qw/domain ns registry/; }
 
 sub transport_protocol_default
