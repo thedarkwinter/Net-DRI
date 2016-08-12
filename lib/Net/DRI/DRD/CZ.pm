d1ccf9cf9e471ebc5641daab22aa49009a633bc3
David Makuni <d.makuni@live.co.uk>
2016-08-12 13:41:33 +0100

    updated & added .CZ EPP extensions / modules

--- lib/Net/DRI/DRD/CZ.pm
+++ lib/Net/DRI/DRD/CZ.pm
@@ -20,7 +20,10 @@ use warnings;
 
 use base qw/Net::DRI::DRD/;
 
+use Net::DRI::Data::Contact::CZ;
 use DateTime::Duration;
+use Net::DRI::Util;
+use Net::DRI::Exception;
 
 =pod
 
@@ -51,7 +54,7 @@ Tonnerre Lombard, E<lt>tonnerre.lombard@sygroup.chE<gt>
 =head1 COPYRIGHT
 
 Copyright (c) 2008,2009 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
-          (c) 2011 Patrick Mevzek <netdri@dotandco.com>.
+           (c) 2011 Patrick Mevzek <netdri@dotandco.com>.
 All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
@@ -65,27 +68,87 @@ See the LICENSE file that comes with this distribution for more details.
 
 ####################################################################################################
 
-sub new
-{
- my $class=shift;
- my $self=$class->SUPER::new(@_);
- $self->{info}->{host_as_attr}=0;
- $self->{info}->{contact_i18n}=2;
- return $self;
+sub new {
+  my $class=shift;
+  my $self=$class->SUPER::new(@_);
+  $self->{info}->{host_as_attr}=0;
+  $self->{info}->{contact_i18n}=2;
+  return $self;
 }
 
-sub periods  { return map { DateTime::Duration->new(years => $_) } (1..10); }
-sub name     { return 'CZ'; }
-sub tlds     { return ('cz'); }
-sub object_types { return ('domain','contact','ns'); }
+sub periods       { return map { DateTime::Duration->new(years => $_) } (1..10); }
+sub name          { return 'CZ'; }
+sub tlds          { return ('cz'); }
+sub object_types  { return ('domain','contact','ns','keyset'); }
 sub profile_types { return qw/epp/; }
 
-sub transport_protocol_default
-{
- my ($self,$type)=@_;
+sub transport_protocol_default {
+  my ($self,$type)=@_;
+  return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::CZ',{}) if $type eq 'epp';
+  return;
+}
 
- return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::CZ',{}) if $type eq 'epp';
- return;
+sub set_factories {
+   my ($self,$po)=@_;
+   $po->factories('contact',sub { return Net::DRI::Data::Contact::CZ->new(@_); });
+   return;
+}
+
+####################################################################################################
+
+####################################################################################################
+
+sub keyset_create {
+  my ($self,$ndr,$keyset,$rd)=@_;
+  return $ndr->process('keyset','create',[$keyset,$rd]);
+}
+
+sub keyset_check {
+  my ($self,$ndr,$keyset,$rd)=@_;
+  return $ndr->process('keyset','check',[$keyset,$rd]);
+}
+
+sub keyset_info {
+  my ($self,$ndr,$keyset,$rd)=@_;
+  return $ndr->process('keyset','info',[$keyset,$rd]);
+}
+
+sub keyset_transfer {
+  my ($self,$ndr,$keyset,$rd)=@_;
+  return $ndr->process('keyset','transfer',[$keyset,$rd]);
+}
+
+sub keyset_delete {
+  my ($self,$ndr,$keyset,$rd)=@_;
+  return $ndr->process('keyset','delete',[$keyset,$rd]);
+}
+
+sub keyset_update {
+   my ($self,$ndr,$domain,$tochange,$rd)=@_;
+   $rd=Net::DRI::Util::create_params('keyset_update',$rd);
+   Net::DRI::Util::check_isa($tochange,'Net::DRI::Data::Changes');
+   Net::DRI::Exception->new(0,'DRD',4,'Registry does not handle contacts')
+     if ($tochange->all_defined('contact') && ! $self->has_object('contact'));
+
+   my $fp=$ndr->protocol->nameversion();
+     foreach my $t ($tochange->types()) {
+      Net::DRI::Exception->die(0,'DRD',5,'Protocol '.$fp.' is not capable of keyset_update/'.$t)
+        unless $ndr->protocol_capable('keyset_update',$t);
+      my $add=$tochange->add($t);
+      my $del=$tochange->del($t);
+      my $set=$tochange->set($t);
+      Net::DRI::Exception->die(0,'DRD',5,'Protocol '.$fp.' is not capable of keyset_update/'.$t.' (add)')
+        if (defined($add) && ! $ndr->protocol_capable('keyset_update',$t,'add'));
+      Net::DRI::Exception->die(0,'DRD',5,'Protocol '.$fp.' is not capable of keyset_update/'.$t.' (del)')
+        if (defined($del) && ! $ndr->protocol_capable('keyset_update',$t,'del'));
+      Net::DRI::Exception->die(0,'DRD',5,'Protocol '.$fp.' is not capable of keyset_update/'.$t.' (set)')
+        if (defined($set) && ! $ndr->protocol_capable('keyset_update',$t,'set'));
+     }
+
+   foreach ($tochange->all_defined('contact')) { Net::DRI::Util::check_isa($_,'Net::DRI::Data::ContactSet'); }
+
+   my $rc=$ndr->process('keyset','update',[$domain,$tochange,$rd]);
+   return $rc;
 }
 
 ####################################################################################################
--- lib/Net/DRI/Protocol/EPP/Extensions/CZ/Contact.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/CZ/Contact.pm
@@ -1,7 +1,7 @@
-## Domain Registry Interface, .CZ Contact EPP extension commands
+## Domain Registry Interface, CZ.NIC Contact EPP extension commands
 ##
-## Copyright (c) 2008,2010,2013 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
-##                    All rights reserved.
+## Copyright (c) 2008,2010,2013 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>. All rights reserved.
+## Copyright (c) 2014-2016 David Makuni <d.makuni@live.co.uk>. All rights reserved.
 ##
 ## This file is part of Net::DRI
 ##
@@ -48,10 +48,13 @@ E<lt>http://www.dotandco.com/services/software/Net-DRI/E<gt>
 =head1 AUTHOR
 
 Tonnerre Lombard, E<lt>tonnerre.lombard@sygroup.chE<gt>
+David Makuni, E<lt>d.makuni@live.co.uk<gt>
 
 =head1 COPYRIGHT
 
 Copyright (c) 2008,2010,2013 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
+Copyright (c) 2014-2016 David Makuni <d.makuni@live.co.uk>.
+
 All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
@@ -65,205 +68,212 @@ See the LICENSE file that comes with this distribution for more details.
 
 ####################################################################################################
 
-sub register_commands
-{
-	my ($class, $version) = @_;
-	my %tmp = ( 
-		info =>   [ undef, \&info_parse ],
-		create => [ \&create, undef ],
-		update => [ \&update, undef ]
-	);
-
-	return { 'contact' => \%tmp };
+sub register_commands {
+  my ($class, $version) = @_;
+  my %tmp = (
+    info =>   [ undef, \&info_parse ],
+    create => [ \&create, undef ],
+    update => [ \&update, undef ]
+  );
+  return { 'contact' => \%tmp };
 }
 
 ####################################################################################################
 
-sub build_command
-{
-	my ($msg, $command, $contact) = @_;
-	my @contact = (ref($contact) eq 'ARRAY')? @$contact : ($contact);
-	my @c = map { Net::DRI::Util::isa_contact($_) ?
-		$_->srid() : $_ } @contact;
+sub build_command {
+  my ($msg, $command, $contact) = @_;
+  my @contact = (ref($contact) eq 'ARRAY')? @$contact : ($contact);
+  my @c = map { Net::DRI::Util::isa_contact($_) ? $_->srid() : $_ } @contact;
 
-	Net::DRI::Exception->die(1,'protocol/EPP',2,'Contact id needed')
-		unless @c;
-	foreach my $n (@c)
-	{
-		Net::DRI::Exception->die(1, 'protocol/EPP', 2,
-			'Contact id needed') unless (defined($n) && $n && !ref($n));
-		Net::DRI::Exception->die(1, 'protocol/EPP', 10,
-			'Invalid contact id: ' . $n)
-			unless Net::DRI::Util::xml_is_token($n, 3, 16);
-	}
+  Net::DRI::Exception->die(1,'protocol/EPP',2,'Contact id needed') unless @c;
 
-	my $tcommand = (ref($command)) ? $command->[0] : $command;
+	foreach my $n (@c) {
+    Net::DRI::Exception->die(1, 'protocol/EPP', 2, 'Contact id needed')
+		  unless (defined($n) && $n && !ref($n));
+    Net::DRI::Exception->die(1, 'protocol/EPP', 10, 'Invalid contact id: ' . $n)
+      unless Net::DRI::Util::xml_is_token($n, 3, 16);
+  }
+
+  my $tcommand = (ref($command)) ? $command->[0] : $command;
 	$msg->command([$command, 'contact:' . $tcommand,
-		sprintf('xmlns:contact="%s" xsi:schemaLocation="%s %s"',$msg->nsattrs('contact'))]);
+    sprintf('xmlns:contact="%s" xsi:schemaLocation="%s %s"',$msg->nsattrs('contact'))]);
 
-	my @d = map { ['contact:id', $_] } @c;
-	if (($tcommand =~ m/^(?:info|transfer)$/) && ref($contact[0]) &&
-		Net::DRI::Util::isa_contact($contact[0]))
-	{
-		my $az = $contact[0]->auth();
-		if ($az && ref($az) && exists($az->{pw}))
-		{
-			push(@d, ['contact:authInfo', $az->{pw}]);
-		}
-	}
+  my @d = map { ['contact:id', $_] } @c;
+  if (($tcommand =~ m/^(?:info|transfer)$/) && ref($contact[0]) &&
+    Net::DRI::Util::isa_contact($contact[0])) {
+    my $az = $contact[0]->auth();
+    if ($az && ref($az) && exists($az->{pw})) {
+      push(@d, ['contact:authInfo', $az->{pw}]);
+    }
+  }
 
-	return @d;
+  return @d;
 }
 
 ############ Query commands
 
-sub info_parse
-{
-	my ($po, $otype, $oaction, $oname, $rinfo) = @_;
-	my $mes = $po->message();
+sub info_parse {
+  my ($po, $otype, $oaction, $oname, $rinfo) = @_;
+  my $mes = $po->message();
 
-	return unless $mes->is_success();
+  return unless $mes->is_success();
 
-	my $infdata = $mes->get_response('contact','infData');
-	return unless $infdata;
+  my $infdata = $mes->get_response('contact','infData');
+  return unless $infdata;
 
-	my $s = $rinfo->{contact}->{$oname}->{self};
-	my $el = $infdata->getElementsByTagNameNS($mes->ns('contact'),
-		'authInfo');
-	while (my $ai = $el->shift())
-	{
-		$s->auth({pw => $ai->getFirstChild()->getData()})
-			if (defined($ai) && defined($ai->getFirstChild()) &&
-				$ai->getFirstChild()->nodeType() == 3);
-	}
-        return;
+  my $s = $rinfo->{contact}->{$oname}->{self};
+
+  foreach my $el (Net::DRI::Util::xml_list_children($infdata)) {
+   my ($name,$c)=@$el;
+   if ($name =~ m/^(vat|ident|notifyEmail)$/) {
+    $s->vat($c->textContent()) if $name eq 'vat';
+    $s->identity({ 'value' => $c->textContent(),
+		  'type' => $c->getAttribute('type') }) if $name eq 'ident';
+    $s->notifyemail($c->textContent()) if $name eq 'notifyEmail';
+   }
+  }
+
+  my $el = $infdata->getElementsByTagNameNS($mes->ns('contact'), 'authInfo');
+
+  while (my $ai = $el->shift()) {
+    $s->auth({pw => $ai->getFirstChild()->getData()})
+    if (defined($ai) && defined($ai->getFirstChild()) &&
+		  $ai->getFirstChild()->nodeType() == 3);
+  }
+
+	return;
 }
 
 ############ Transform commands
 
-sub build_authinfo
-{
-	my $contact = shift;
-	my $az = $contact->auth();
-	return () unless ($az && ref($az) && exists($az->{pw}));
-	return ['contact:authInfo', $az->{pw}];
+sub build_authinfo {
+  my $contact = shift;
+  my $az = $contact->auth();
+  return () unless ($az && ref($az) && exists($az->{pw}));
+  return ['contact:authInfo', $az->{pw}];
 }
 
-sub build_disclose
-{
-	my $contact = shift;
-	my $d=$contact->disclose();
-	return () unless ($d && ref($d));
-	my %v=map { $_ => 1 } values(%$d);
-	return () unless (keys(%v)==1); ## 1 or 0 as values, not both at same time
+sub build_disclose {
+  my $contact = shift;
+  my $d=$contact->disclose();
+  return () unless ($d && ref($d));
+  my %v=map { $_ => 1 } values(%$d);
+  return () unless (keys(%v)==1); ## 1 or 0 as values, not both at same time
 
-	my @d;
-	push(@d, ['contact:name']) if (exists($d->{name}));
-	push(@d, ['contact:org']) if (exists($d->{org}));
-	push(@d, ['contact:addr']) if (exists($d->{addr}));
-	push(@d, ['contact:voice']) if (exists($d->{voice}));
-	push(@d, ['contact:fax']) if (exists($d->{fax}));
-	push(@d, ['contact:email']) if (exists($d->{email}));
+  my @d;
+  push(@d, ['contact:name']) if (exists($d->{name}));
+  push(@d, ['contact:org']) if (exists($d->{org}));
+  push(@d, ['contact:addr']) if (exists($d->{addr}));
+  push(@d, ['contact:voice']) if (exists($d->{voice}));
+  push(@d, ['contact:fax']) if (exists($d->{fax}));
+  push(@d, ['contact:email']) if (exists($d->{email}));
+  push(@d, ['contact:vat']) if (exists($d->{vat}));
+  push(@d, ['contact:ident']) if (exists($d->{identity}));
+  push(@d, ['contact:notifyEmail']) if (exists($d->{notifyemail}));
 
-	return ['contact:disclose',@d,{flag=>(keys(%v))[0]}];
+  return ['contact:disclose',@d,{flag=>(keys(%v))[0]}];
 }
 
-sub build_cdata
-{
-	my ($contact, $v) = @_;
-	my (@post, @addr);
-	my @tmp;
-	my @d;
+sub build_cdata {
+  my ($contact, $v) = @_;
+  my (@post, @addr, $ident);
+  my @tmp;
+  my @d;
 
-	@tmp = $contact->name();
-	if (defined($tmp[0])) { push(@post, ['contact:name', $tmp[0]]); }
+  @tmp = $contact->name();
+  if (defined($tmp[0])) { push(@post, ['contact:name', $tmp[0]]); }
 
-	@tmp = $contact->org();
-	if (defined($tmp[0])) { push(@post, ['contact:org', $tmp[0]]); }
+  @tmp = $contact->org();
+  if (defined($tmp[0])) { push(@post, ['contact:org', $tmp[0]]); }
 
-	@tmp = $contact->street();
-	if (defined($tmp[0])) { foreach (@{$tmp[0]}) {
-		push(@addr, ['contact:street', $_]);
-	} }
+  @tmp = $contact->street();
+  if (defined($tmp[0])) { foreach (@{$tmp[0]}) {
+    push(@addr, ['contact:street', $_]);
+  } }
 
-	@tmp = $contact->city();
-	if (defined($tmp[0])) { push(@addr, ['contact:city', $tmp[0]]); }
+  @tmp = $contact->city();
+  if (defined($tmp[0])) { push(@addr, ['contact:city', $tmp[0]]); }
 
-	@tmp = $contact->sp();
-	if (defined($tmp[0])) { push(@addr, ['contact:sp', $tmp[0]]); }
+  @tmp = $contact->sp();
+  if (defined($tmp[0])) { push(@addr, ['contact:sp', $tmp[0]]); }
 
-	@tmp = $contact->pc();
-	if (defined($tmp[0])) { push(@addr, ['contact:pc', $tmp[0]]); }
+  @tmp = $contact->pc();
+  if (defined($tmp[0])) { push(@addr, ['contact:pc', $tmp[0]]); }
 
-	@tmp = $contact->cc();
-	if (defined($tmp[0])) { push(@addr, ['contact:cc', $tmp[0]]); }
+  @tmp = $contact->cc();
+  if (defined($tmp[0])) { push(@addr, ['contact:cc', $tmp[0]]); }
 
-	push(@post, ['contact:addr', @addr]) if (@addr);
-	push(@d, ['contact:postalInfo', @post]);
-	push(@d, Net::DRI::Protocol::EPP::Util::build_tel('contact:voice', $contact->voice()))
-		if (defined($contact->voice()));
-	push(@d, Net::DRI::Protocol::EPP::Util::build_tel('contact:fax', $contact->fax()))
-		if (defined($contact->fax()));
-	push(@d, ['contact:email', $contact->email()])
-		if (defined($contact->email()));
-	push(@d, build_authinfo($contact));
-	push(@d, build_disclose($contact));
+  push(@post, ['contact:addr', @addr]) if (@addr);
+  push(@d, ['contact:postalInfo', @post]);
+  push(@d, Net::DRI::Protocol::EPP::Util::build_tel('contact:voice', $contact->voice()))
+    if (defined($contact->voice()));
+  push(@d, Net::DRI::Protocol::EPP::Util::build_tel('contact:fax', $contact->fax()))
+    if (defined($contact->fax()));
+  push(@d, ['contact:email', $contact->email()])
+    if (defined($contact->email()));
+  push(@d, ['contact:vat', $contact->vat()])
+    if (defined($contact->vat()));
+  push(@d, ['contact:ident', { type => $contact->identity()->{type} },
+	  $contact->identity()->{value}])
+      if (defined($contact->identity()));
+  push(@d, ['contact:notifyEmail', $contact->notifyemail()])
+    if (defined($contact->notifyemail()));
 
-	return @d;
+  push(@d, build_authinfo($contact));
+  push(@d, build_disclose($contact));
+
+  return @d;
 }
 
-sub create 
-{
-	my ($epp, $contact) = @_;
-	my $mes = $epp->message();
-	my @d = build_command($mes, 'create', $contact);
+sub create {
+  my ($epp, $contact) = @_;
+  my $mes = $epp->message();
+  my @d = build_command($mes, 'create', $contact);
 
-	Net::DRI::Exception->die(1, 'protocol/EPP', 10, 'Invalid contact ' .
-		$contact) unless Net::DRI::Util::isa_contact($contact);
-	$contact->validate(); ## will trigger an Exception if needed
-	push(@d, build_cdata($contact, $epp->{contacti18n}));
-	$mes->command_body(\@d);
-        return;
+  Net::DRI::Exception->die(1, 'protocol/EPP', 10, 'Invalid contact ' . $contact)
+	  unless Net::DRI::Util::isa_contact($contact);
+  $contact->validate(); ## will trigger an Exception if needed
+  push(@d, build_cdata($contact, $epp->{contacti18n}));
+  $mes->command_body(\@d);
+
+	return;
 }
 
-sub update 
-{
-	my ($epp, $contact, $todo) = @_;
-	my $mes = $epp->message();
+sub update {
+  my ($epp, $contact, $todo) = @_;
+  my $mes = $epp->message();
 
-	Net::DRI::Exception::usererr_invalid_parameters($todo .
-		' must be a Net::DRI::Data::Changes object')
-		unless Net::DRI::Util::isa_changes($todo);
-	if ((grep { ! /^(?:add|del)$/ } $todo->types('status')) ||
-		(grep { ! /^(?:set)$/ } $todo->types('info')))
-	{
-		Net::DRI::Exception->die(0, 'protocol/EPP', 11,
-			'Only status add/del or info set available for ' .
-			'contact');
-	}
+  Net::DRI::Exception::usererr_invalid_parameters($todo .
+    ' must be a Net::DRI::Data::Changes object')
+      unless Net::DRI::Util::isa_changes($todo);
 
-	my @d = build_command($mes, 'update', $contact);
+  if ((grep { ! /^(?:add|del)$/ } $todo->types('status')) ||
+    (grep { ! /^(?:set)$/ } $todo->types('info'))) {
+    Net::DRI::Exception->die(0, 'protocol/EPP', 11,
+      'Only status add/del or info set available for contact');
+  }
 
-	my $sadd = $todo->add('status');
-	my $sdel = $todo->del('status');
-	push(@d, ['contact:add', $sadd->build_xml('contact:status')])
-		if ($sadd);
-	push(@d, ['contact:rem', $sdel->build_xml('contact:status')])
-		if ($sdel);
+  my @d = build_command($mes, 'update', $contact);
 
-	my $newc = $todo->set('info');
-	if ($newc)
-	{
-		Net::DRI::Exception->die(1, 'protocol/EPP', 10,
-			'Invalid contact ' . $newc)
-			unless Net::DRI::Util::isa_contact($newc);
-		$newc->validate(1); ## will trigger an Exception if needed
-		my @c = build_cdata($newc, $epp->{contacti18n});
-		push(@d, ['contact:chg', @c]) if (@c);
-	}
+  my $sadd = $todo->add('status');
+  my $sdel = $todo->del('status');
+  push(@d, ['contact:add', $sadd->build_xml('contact:status')])
+    if ($sadd);
+  push(@d, ['contact:rem', $sdel->build_xml('contact:status')])
+    if ($sdel);
 
-	$mes->command_body(\@d);
-        return;
+  my $newc = $todo->set('info');
+  if ($newc) {
+    Net::DRI::Exception->die(1, 'protocol/EPP', 10, 'Invalid contact ' . $newc)
+      unless Net::DRI::Util::isa_contact($newc);
+    $newc->validate(1); ## will trigger an Exception if needed
+    my @c = build_cdata($newc, $epp->{contacti18n});
+    push(@d, ['contact:chg', @c]) if (@c);
+  }
+
+  $mes->command_body(\@d);
+
+	return;
 }
 
 ####################################################################################################
