f715c2137cca48f834819c065692838d3f8b455d
Michael Holloway <michael@thedarkwinter.com>
2017-05-16 09:28:13 +0200

    replicated updated DRD renames (no merge)

--- lib/Net/DRI/DRD/IIS.pm
+++ lib/Net/DRI/DRD/IIS.pm
@@ -1,3 +1,5 @@
+## NOTE: This module is depricated in favour of the Net::DRI::DRD::IIS driver which supports both .SE and .NU domains
+
 ## Domain Registry Interface, .IIS policy on reserved names
 ## Contributed by Elias Sidenbladh and Ulrich Wisser from NIC SE
 ## Updated 2014 Michael Holloway to change regerences to IIS for inclusion of .NU
@@ -14,57 +16,22 @@
 ## See the LICENSE file that comes with this distribution for more details.
 ####################################################################################################
 
-package Net::DRI::DRD::IIS;
+package Net::DRI::DRD::SE;
 
 use strict;
 use warnings;
 
-use base qw/Net::DRI::DRD/;
-
-use DateTime::Duration;
-use Net::DRI::Util;
-use Net::DRI::Data::Contact::IIS;
-
-## Only transfer requests and queries are possible, the rest is handled "off line".
-__PACKAGE__->make_exception_for_unavailable_operations(qw/domain_transfer_stop domain_transfer_accept domain_transfer_refuse domain_delete/);
+use base qw/Net::DRI::DRD::IIS/;
 
 =pod
 
 =head1 NAME
 
-Net::DRI::DRD::IIS - .SE policies for Net::DRI
+Net::DRI::DRD::SE - .SE policies for Net::DRI
 
 =head1 DESCRIPTION
 
-Please see the README file for details.
-
-=head1 SUPPORT
-
-For now, support questions should be sent to:
-
-E<lt>netdri@dotandco.comE<gt>
-
-Please also see the SUPPORT file in the distribution.
-
-=head1 SEE ALSO
-
-E<lt>http://www.dotandco.com/services/software/Net-DRI/E<gt>
-
-=head1 AUTHOR
-
-Patrick Mevzek, E<lt>netdri@dotandco.comE<gt>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2006-2011,2013 Patrick Mevzek <netdri@dotandco.com>.
-All rights reserved.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-See the LICENSE file that comes with this distribution for more details.
+NOTE: This module is depricated in favour of the L<Net::DRI::DRD::IIS> driver which supports both .SE and .NU domains
 
 =cut
 
@@ -73,62 +40,10 @@ See the LICENSE file that comes with this distribution for more details.
 sub new
 {
  my $class=shift;
- my $self=$class->SUPER::new(@_);
- $self->{info}->{host_as_attr}=0;
- return $self;
+ warn "DRD::SE.pm is depricated, please use DRD::IIS.pm for .SE and .NU domains!";
+ return $class->SUPER::new(@_);
 }
 
-sub name     { return 'IIS'; }
-sub tlds     { return ('se','tm.se','org.se','pp.se','parti.se','presse.se','nu'); }
-sub periods  { return map { DateTime::Duration->new(months => $_) } (12..120); }
-sub object_types { return ('domain','contact','ns'); }
-sub profile_types { return qw/epp whois/; }
+sub name { return 'SE'; }
 
-sub transport_protocol_default
-{
- my ($self,$type)=@_;
-
- return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::IIS',{})                 if $type eq 'epp';
- return ('Net::DRI::Transport::Socket',{remote_host=>'whois.nic-se.se'},'Net::DRI::Protocol::Whois',{}) if $type eq 'whois';
- return;
-}
-
-sub set_factories
-{
- my ($self,$po)=@_;
- $po->factories('contact',sub { return Net::DRI::Data::Contact::IIS->new(@_); });
- return;
-}
-
-####################################################################################################
-
-sub verify_name_domain
-{
- my ($self,$ndr,$domain,$op)=@_;
- return $self->_verify_name_rules($domain,$op,{check_name => 1,
-                                               my_tld => 1,
-                                              });
-}
-
-sub verify_duration_create
-{
- my ($self,$ndr,$duration,$domain)=@_;
-
- return 0 unless defined $duration;
-
- my $m=$duration->in_units('months');
- return ($m >= 12 && $m <= 120 )? 0 : 1; ## Only 12 - 120 months allowed
-}
-
-sub verify_duration_renew
-{
- my ($self,$ndr,$duration,$domain,$curexp)=@_;
-
- return 0 unless defined $duration;
-
- my $m=$duration->in_units('months');
- return ($m >= 12 && $m <= 120 )? 0 : 1; ## Only 12 - 120 months allowed
-}
-
-####################################################################################################
 1;
--- lib/Net/DRI/DRD/ZACR.pm
+++ lib/Net/DRI/DRD/ZACR.pm
@@ -1,7 +1,6 @@
-## Domain Registry Interface, ZACR (ZA Central Registry) GTLD Driver
+## Domain Registry Interface, CO.ZA policy on reserved names
 ##
-## Copyright (c) 2013 Patrick Mevzek <netdri@dotandco.com>. All rights reserved.
-##           (c) 2013 Michael Holloway <michael@thedarkwinter.com>. All rights reserved.
+## Copyright (c) 2011,2012 Patrick Mevzek <netdri@dotandco.com>. All rights reserved.
 ##
 ## This file is part of Net::DRI
 ##
@@ -11,38 +10,30 @@
 ## (at your option) any later version.
 ##
 ## See the LICENSE file that comes with this distribution for more details.
-####################################################################################################
+#########################################################################################
 
-package Net::DRI::DRD::ZACR;
+package Net::DRI::DRD::COZA;
 
 use strict;
 use warnings;
 
 use base qw/Net::DRI::DRD/;
 
+use Net::DRI::Util;
+use Net::DRI::Exception;
 use DateTime::Duration;
 
-__PACKAGE__->make_exception_for_unavailable_operations(qw/host_update host_current_status host_check host_exist host_delete host_create host_info/);
+__PACKAGE__->make_exception_for_unavailable_operations(qw/contact_transfer contact_transfer_start contact_transfer_stop contact_transfer_query contact_transfer_accept contact_transfer_refuse/);
 
 =pod
 
 =head1 NAME
 
-Net::DRI::DRD::ZACR - ZA Central Registry Driver for Net::DRI
+Net::DRI::DRD::COZA - CO.ZA policies for Net::DRI
 
 =head1 DESCRIPTION
 
-Additional domain extension ZACR New Generic TLDs. Note this is separate from COZA ccTLD Driver as the systems differ.
-
-ZACR utilises the following standard, and custom extensions. Please see the test files for more examples.
-
-=head2 Standard extensions:
-
-=head3 L<Net::DRI::Protocol::EPP::Extensions::secDNS> urn:ietf:params:xml:ns:secDNS-1.1
-
-=head2 Custom extensions:
-
-=head3 L<Net::DRI::Protocol::EPP::Extensions::COZA::Domain> http://co.za/epp/extensions/cozadomain-1-0
+Please see the README file for details.
 
 =head1 SUPPORT
 
@@ -58,12 +49,11 @@ E<lt>http://www.dotandco.com/services/software/Net-DRI/E<gt>
 
 =head1 AUTHOR
 
-Michael Holloway, E<lt>michael@thedarkwinter.comE<gt>
+Patrick Mevzek, E<lt>netdri@dotandco.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2013 Patrick Mevzek <netdri@dotandco.com>.
-          (c) 2013 Michael Holloway <michael@thedarkwinter.com>.
+Copyright (c) 2011,2012 Patrick Mevzek <netdri@dotandco.com>.
 All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
@@ -75,31 +65,43 @@ See the LICENSE file that comes with this distribution for more details.
 
 =cut
 
-####################################################################################################
+#####################################################################################
 
 sub new
 {
  my $class=shift;
  my $self=$class->SUPER::new(@_);
  $self->{info}->{host_as_attr}=1;
- $self->{info}->{contact_i18n}=4; ## LOC+INT
+ $self->{info}->{contact_i18n}=1; ## LOC only ## Documentation does not say exactly what is allowed
+ bless($self,$class);
  return $self;
 }
 
-sub periods  { return map { DateTime::Duration->new(years => $_) } (1); }
-sub name     { return 'ZACR'; }
-sub tlds     { return ('cities.dnservices.co.za','joburg','durban','capetown','africa','wien'); } # FIXME : the first is for OT&E tlds, the rest for live.
-sub object_types { return ('domain','contact'); }
+sub periods  { return map { DateTime::Duration->new(years => $_) } (1); } ## unknown precisely
+sub name     { return 'COZA'; }
+sub tlds     { return (map { $_.'.za' } qw /co net org web/); }
+sub object_types { return (qw/domain contact/); }
 sub profile_types { return qw/epp/; }
 
 sub transport_protocol_default
 {
  my ($self,$type)=@_;
 
- return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::ZACR',{}) if $type eq 'epp';
+ return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::COZA',{}) if $type eq 'epp';
  return;
 }
 
-####################################################################################################
+######################################################################################
 
+sub registrar_balance
+{
+ my ($self,$ndr,$rd)=@_;
+ if (!defined $rd) {
+   $rd = ( defined($self->info('client_id'))?$self->info('client_id') :$self->info('clid') );
+ }
+ my $rc=$ndr->process('contact','info',[$ndr->local_object('contact')->srid($rd),{balance=>1}]);
+ return $rc;
+}
+
+#################################################################################################################
 1;
