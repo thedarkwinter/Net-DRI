d506ae30a96b1b4955f64183624faf296e263ede
Michael Holloway <michael@thedarkwinter.com>
2017-05-29 15:48:31 +0200

    added KNET drd

---
+++ lib/Net/DRI/DRD/KNET.pm
@@ -0,0 +1,120 @@
+## Domain Registry Interface, KNET Registry GTLD Driver for multiple TLDs
+##
+## Copyright (c) 2008-2010 Patrick Mevzek <netdri@dotandco.com>. All rights reserved.
+## Copyright (c) 2017 Michael Holloway <michael@thedarkwinter.com>. All rights reserved.
+##
+## This file is part of Net::DRI
+##
+## Net::DRI is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## See the LICENSE file that comes with this distribution for more details.
+#########################################################################################
+
+package Net::DRI::DRD::KNET;
+
+use strict;
+use warnings;
+
+use base qw/Net::DRI::DRD/;
+
+use DateTime::Duration;
+
+=pod
+
+=head1 NAME
+
+Net::DRI::DRD::KNET - KNET Registry GTLD driver for Net::DRI
+
+=head1 DESCRIPTION
+
+Additional domain extensions used by KNET Generic TLDs
+
+KNET utilises the following standard extensions. Please see the test files for more examples.
+
+=head2 Standard extensions:
+
+=head3 L<Net::DRI::Protocol::EPP::Extensions::secDNS> urn:ietf:params:xml:ns:secDNS-1.1
+
+=head3 L<Net::DRI::Protocol::EPP::Extensions::GracePeriod> urn:ietf:params:xml:ns:rgp-1.0
+
+=head3 L<Net::DRI::Protocol::EPP::Extensions::LaunchPhase> urn:ietf:params:xml:ns:launch-1.0
+
+=head3 L<Net::DRI::Protocol::EPP::Extensions::IDN> urn:ietf:params:xml:ns:idn-1.0
+
+=head2 Custom Extensions
+
+=head3 L<Net::DRI::Protocol::EPP::Extensions::CentralNic::Fee> urn:centralnic:params:xml:ns:fee-0.7
+
+=head1 SUPPORT
+
+For now, support questions should be sent to:
+
+E<lt>netdri@dotandco.comE<gt>
+
+Please also see the SUPPORT file in the distribution.
+
+=head1 SEE ALSO
+
+E<lt>http://www.dotandco.com/services/software/Net-DRI/E<gt>
+
+=head1 AUTHOR
+
+Patrick Mevzek, E<lt>netdri@dotandco.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2008-2010 Patrick Mevzek <netdri@dotandco.com>.
+Copyright (c) 2017 Michael Holloway <michael@thedarkwinter.com>
+All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+See the LICENSE file that comes with this distribution for more details.
+
+=cut
+
+####################################################################################################
+
+sub new
+{
+ my $class=shift;
+ my $self=$class->SUPER::new(@_);
+ $self->{info}->{host_as_attr}=0;
+ $self->{info}->{contact_i18n}=4; ## LOC+INT
+ return $self;
+}
+
+sub periods      { return map { DateTime::Duration->new(years => $_) } (1..10); }
+sub name         { return 'CoCCA::GTLD'; }
+sub tlds         { return (qw/xn--45q11c xn--3bst00m xn--ses554g xn--efvy88h xn--czr694b xn--czru2d xn--6qq986b3xl xn--30rr7y xn--imr513n xn--otu796d xn--9et52u xn--hxt814e wang top/); }
+sub object_types { return ('domain','ns','contact'); }
+sub profile_types { return qw/epp/; }
+
+sub transport_protocol_default
+{
+ my ($self,$type)=@_;
+
+ return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::NEWGTLD',{custom => ['CentralNic::Fee'], 'brown_fee_version' => '0.8'}) if $type eq 'epp';
+ return;
+}
+
+####################################################################################################
+
+sub verify_name_domain
+{
+ my ($self,$ndr,$domain,$op)=@_;
+ return $self->_verify_name_rules($domain,$op,{check_name => 1,
+                                               my_tld => 1,
+                                               icann_reserved => 1,
+                                              });
+}
+
+####################################################################################################
+
+1;
