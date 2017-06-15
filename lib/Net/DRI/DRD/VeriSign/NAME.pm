c7bd7ef4918e9be5493ae70b11479e6af8ce4e4a
Michael Holloway <michael@thedarkwinter.com>
2017-06-15 16:10:17 +0200

    remove odd name message

--- lib/Net/DRI/DRD/VeriSign/NAME.pm
+++ lib/Net/DRI/DRD/VeriSign/NAME.pm
@@ -100,7 +100,6 @@ sub transport_protocol_default
 sub verify_name_domain
 {
   my ( $self, $ndr, $domain, $op ) = @_;
-  print "why are we not calling me?\n";
  return $self->_verify_name_rules($domain,$op,{check_name => 1,
       check_name_dots   => [ 1, 2 ],
       my_tld_not_strict => 1,          ## we need less strict checks because in X.Y.name domain names both X and Y are variables
