7075f47907d75acd1f77458102abc23e8872125e
Paulo Jorge <paullojorgge@gmail.com>
2017-12-07 09:33:19 +0000

    migrate co.nl into centralnic profile

--- lib/Net/DRI/DRD/NGTLD.pm
+++ lib/Net/DRI/DRD/NGTLD.pm
@@ -357,7 +357,7 @@ L<Net::DRI::Protocol::EPP::Extensions::CentralNic::Fee> urn:centralnic:params:xm
     my @coms = (map { $_.'.com' } qw/africa ar br cn co de eu gb gr hu jpn kr mex no qc ru sa se uk us uy za/);
     my @nets = (map { $_.'.net' } qw/uk se gb jp hu in/);
     my @orgs = (map { $_.'.org' } qw/us ae/);
-    my @others = qw/pw com.de com.se/;
+    my @others = qw/pw com.de com.se co.nl/;
     my @ngtlds = qw/art bar basketball budapest college contact design fan fans feedback forum fun host ink love luxe observer online pid press protection realty reit rent rest rugby security site space storage store tech theatre tickets website wiki wme xyz/;
     my @ngtlds_contested = qw/hotel gay mail llc/; # some of these might go to other registries
     my @tlds = (@coms,@nets,@orgs,@others,@ngtlds);
