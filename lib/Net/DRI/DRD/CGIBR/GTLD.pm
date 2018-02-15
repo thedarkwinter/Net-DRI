fa166ad0410cdd18c16b79a04607ed961955bbdb
Paulo Jorge <paullojorgge@gmail.com>
2018-02-15 13:42:42 +0000

    fix host attr for cgibr gtld

--- lib/Net/DRI/DRD/CGIBR/GTLD.pm
+++ lib/Net/DRI/DRD/CGIBR/GTLD.pm
@@ -79,7 +79,7 @@ sub new
 {
  my $class=shift;
  my $self=$class->SUPER::new(@_);
- $self->{info}->{host_as_attr}=0;
+ $self->{info}->{host_as_attr}=1;
  $self->{info}->{contact_i18n}=1; ## LOC only
  return $self;
 }
