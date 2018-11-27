e99e55da6fae338460b7f072bca4bf1f5363c139
paullojorgge <paullojorgge@gmail.com>
2018-11-27 15:13:12 +0000

    restore CloudRegistry units from master

--- lib/Net/DRI/Protocol/EPP/Extensions/CloudRegistry.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/CloudRegistry.pm
@@ -20,27 +20,44 @@ use warnings;
 use base qw/Net::DRI::Protocol::EPP/;
 
 =pod
+
 =head1 NAME
+
 Net::DRI::Protocol::EPP::Extensions::CloudRegistry - Cloud Registry EPP extensions for Net::DRI
+
 =head1 DESCRIPTION
+
 Please see the README file for details.
+
 =head1 SUPPORT
+
 For now, support questions should be sent to:
+
 E<lt>wil@cloudregistry.netE<gt>
+
 Please also see the SUPPORT file in the distribution.
+
 =head1 SEE ALSO
+
 E<lt>http://www.cloudregistry.net/E<gt> and
 E<lt>http://www.dotandco.com/services/software/Net-DRI/E<gt>
+
 =head1 AUTHOR
+
 Wil Tan E<lt>wil@cloudregistry.netE<gt>
+
 =head1 COPYRIGHT
+
 Copyright (c) 2009-2011 Cloud Registry Pty Ltd <http://www.cloudregistry.net>.
 All rights reserved.
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
+
 See the LICENSE file that comes with this distribution for more details.
+
 =cut
 
 ####################################################################################################
