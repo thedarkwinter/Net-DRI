acdc45890cb0a8fdaa6a63a3cc9a7de7a8354ee5
Paulo Jorge <paullojorgge@gmail.com>
2019-03-08 09:28:33 +0000

    UniRegistry::INC tweaks (#331)
    * remove .inc from uniregistry main profile
    
    * enforce fee version

--- lib/Net/DRI/DRD/UniRegistry/UniRegistry.pm
+++ lib/Net/DRI/DRD/UniRegistry/UniRegistry.pm
@@ -104,7 +104,7 @@ sub new
 sub periods  { return map { DateTime::Duration->new(years => $_) } (1..10); }
 sub name     { return 'UniRegistry::UniRegistry'; }
 
-sub tlds     { return ('audio', 'auto', 'blackfriday', 'car', 'cars', 'christmas', 'click', 'country', 'diet', 'flowers', 'game', 'gift', 'guitars', 'help', 'hiphop', 'hiv', 'hosting', 'inc', 'juegos', 'link', 'lol', 'mom', 'photo', 'pics', 'property', 'sexy', 'tattoo'); }
+sub tlds     { return ('audio', 'auto', 'blackfriday', 'car', 'cars', 'christmas', 'click', 'country', 'diet', 'flowers', 'game', 'gift', 'guitars', 'help', 'hiphop', 'hiv', 'hosting', 'juegos', 'link', 'lol', 'mom', 'photo', 'pics', 'property', 'sexy', 'tattoo'); }
 sub object_types { return ('domain','contact','ns'); }
 sub profile_types { return qw/epp/; }
 
@@ -112,7 +112,7 @@ sub transport_protocol_default
 {
  my ($self,$type)=@_;
 
- return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::UniRegistry',{}) if $type eq 'epp';
+ return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::UniRegistry',{ 'brown_fee_version' => '0.7' }) if $type eq 'epp';
  return;
 }
 
