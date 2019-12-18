6e5841ed161cbb368b6dee8dbf59fe9a87508068
Michael Holloway <michael@thedarkwinter.com>
2019-12-18 10:20:35 +0100

    Verisign enabled ChangePoll with test

--- lib/Net/DRI/Protocol/EPP/Extensions/VeriSign/Platforms/NameStore.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/VeriSign/Platforms/NameStore.pm
@@ -78,7 +78,7 @@ sub default_extensions
 {
  my ($self,$rp)=@_;
  $self->{brown_fee_version} = $rp->{brown_fee_version} if exists $rp->{brown_fee_version};
- my @c=qw/VeriSign::Sync VeriSign::PollLowBalance VeriSign::PollRGP VeriSign::IDNLanguage VeriSign::WhoWas VeriSign::Suggestion VeriSign::Balance GracePeriod SecDNS LaunchPhase VeriSign::DefReg VeriSign::EmailFwd/;
+ my @c=qw/VeriSign::Sync VeriSign::PollLowBalance VeriSign::PollRGP VeriSign::IDNLanguage VeriSign::WhoWas VeriSign::Suggestion VeriSign::Balance GracePeriod SecDNS ChangePoll LaunchPhase VeriSign::DefReg VeriSign::EmailFwd/;
  push @c,'VeriSign::JobsContact' if exists $rp->{default_product} && defined $rp->{default_product} && $rp->{default_product} eq 'dotJOBS';
  push @c,qw/VeriSign::PremiumDomain CentralNic::Fee/; ## not active for all TLDs, a little complicated
  push @c,'VeriSign::NameStore'; ## this must come last
