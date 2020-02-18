80584ec8ef6137a005b22297aaa1c245c3bee225
Paulo Jorge <paullojorgge@gmail.com>
2020-02-18 15:08:13 +0000

    .whoswho transition from neustar to tango

--- lib/Net/DRI/DRD/TangoRS/TangoRS.pm
+++ lib/Net/DRI/DRD/TangoRS/TangoRS.pm
@@ -94,7 +94,7 @@ sub new
 
 sub periods  { return map { DateTime::Duration->new(years => $_) } (1..10); }
 sub name     { return 'TangoRS::TangoRS'; }
-sub tlds     { return (qw/ruhr gmx ifm nrw/); }
+sub tlds     { return (qw/ruhr gmx ifm nrw whoswho/); }
 sub object_types { return ('domain','contact','ns'); }
 sub profile_types { return qw/epp/; }
 
