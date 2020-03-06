5369322077643fa18fa0e439b9ace813cdbf203d
Paulo Jorge <paullojorgge@gmail.com>
2020-03-06 11:12:31 +0000

    enforce usage of TLSv1.2 for NORID profile

--- lib/Net/DRI/DRD/NORID.pm
+++ lib/Net/DRI/DRD/NORID.pm
@@ -90,7 +90,7 @@ sub profile_types { return qw/epp/; }
 sub transport_protocol_default {
     my ($self,$type)=@_;
 
-    return ('Net::DRI::Transport::Socket',{},'Net::DRI::Protocol::EPP::Extensions::NO',{}) if $type eq 'epp';
+    return ('Net::DRI::Transport::Socket',{ 'ssl_version' => 'TLSv12', 'ssl_cipher_list' => undef },'Net::DRI::Protocol::EPP::Extensions::NO',{}) if $type eq 'epp';
 # suppress until whois is supported
 #return ('Net::DRI::Transport::Socket',{remote_host=>'whois.norid.no'},'Net::DRI::Protocol::Whois',{}) if $type eq 'whois';
 
