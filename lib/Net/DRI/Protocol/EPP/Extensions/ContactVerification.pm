6f2653e7ab2509903e3a24a711c1df887c953289
Paulo Jorge <paullojorgge@gmail.com>
2020-04-03 12:15:54 +0100

    ContactVerification

--- lib/Net/DRI/Protocol/EPP/Extensions/ContactVerification.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/ContactVerification.pm
@@ -1,6 +1,6 @@
 ## Domain Registry Interface, Contact Verification Extension Mapping for EPP
 ##
-## Copyright (c) 2016 Patrick Mevzek <netdri@dotandco.com>. All rights reserved.
+## Copyright (c) 2016,2018 Patrick Mevzek <netdri@dotandco.com>. All rights reserved.
 ##
 ## This file is part of Net::DRI
 ##
@@ -36,7 +36,7 @@ sub register_commands
 sub setup
 {
  my ($class,$po,$version)=@_;
- state $rns = { 'vericontact' => [ 'urn:ietf:params:xml:ns:vericontact-1.0','vericontact-1.0.xsd' ] };
+ state $rns = { 'vericontact' => 'urn:ietf:params:xml:ns:vericontact-1.0' };
  $po->ns($rns);
  return;
 }
@@ -51,7 +51,7 @@ sub check_parse
  my $mes=$po->message();
  return unless $mes->is_success();
 
- my $data=$mes->get_extension($mes->ns('vericontact'),'chkData');
+ my $data=$mes->get_extension('vericontact', 'chkData');
  return unless defined $data;
 
  my %r;
@@ -75,7 +75,7 @@ sub info_parse
  return unless $mes->is_success();
 
  my $ns=$mes->ns('vericontact');
- my $data=$mes->get_extension($ns,'infData');
+ my $data=$mes->get_extension('vericontact', 'infData');
  return unless defined $data;
 
  my %r;
@@ -135,7 +135,7 @@ Patrick Mevzek, E<lt>netdri@dotandco.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2016 Patrick Mevzek <netdri@dotandco.com>.
+Copyright (c) 2016,2018 Patrick Mevzek <netdri@dotandco.com>.
 All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
