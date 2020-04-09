5baf6c13db703b1b1b194a126bf2476029e4e83b
Paulo Jorge <paullojorgge@gmail.com>
2020-04-09 10:32:01 +0100

    Afilias::AV

--- lib/Net/DRI/Protocol/EPP/Extensions/Afilias/AV.pm
+++ lib/Net/DRI/Protocol/EPP/Extensions/Afilias/AV.pm
@@ -1,7 +1,7 @@
 ## Domain Registry Interface, .PRO A/V extensions
 ##
-## Copyright (c) 2008,2009,2013 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
-##                    All rights reserved.
+## Copyright (c) 2008,2009,2013 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>. All rights reserved.
+## Copyright (c) 2020 Paulo Jorge <paullojorgge@gmail.com>. All rights reserved.
 ##
 ## This file is part of Net::DRI
 ##
@@ -54,6 +54,7 @@ E<lt>http://www.hexonet.net/E<gt>.
 =head1 COPYRIGHT
 
 Copyright (c) 2008,2009,2013 Tonnerre Lombard <tonnerre.lombard@sygroup.ch>.
+Copyright (c) 2020 Paulo Jorge <paullojorgge@gmail.com>.
 All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
@@ -82,7 +83,7 @@ sub register_commands
 sub setup
 {
  my ($class,$po,$version)=@_;
- $po->ns({ av  => ['http://registrypro.pro/2003/epp/1/av-2.0', 'av-2.0.xsd'] });
+ $po->ns({ av  => 'http://registrypro.pro/2003/epp/1/av-2.0' });
  return;
 }
 
@@ -107,7 +108,7 @@ sub build_command
 	}
 
 	my $tcommand = (ref($command) ? $command->[0] : $command);
-	$msg->command([$command, 'av:' . $tcommand,sprintf('xmlns:av="%s" xsi:schemaLocation="%s %s"',$msg->nsattrs('av'))]);
+	$msg->command([$command, 'av:' . $tcommand,sprintf('xmlns:av="%s"',$msg->ns('av'))]);
 	return map { ['av:id', $_, $domainattr] } @dom;
 }
 
